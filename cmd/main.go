fcd4e68de48e04737c735004bfbe63b11f6b0770
louisevanderlith <louisevanderlith@gmail.com>
2020-10-19 20:40:50 +0200

    Updated CMS pages

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"github.com/louisevanderlith/leads/core"
+	"github.com/louisevanderlith/leads/handles"
+	"net/http"
+	"time"
+)
+
+func main() {
+	securty := flag.String("security", "http://localhost:8086", "Security Provider's URL")
+	manager := flag.String("manager", "http://localhost:8097", "Security Provider's URL")
+	srcSecrt := flag.String("scopekey", "secret", "Secret used to validate against scopes")
+	flag.Parse()
+
+	core.CreateContext()
+	defer core.Shutdown()
+
+	srvr := &http.Server{
+		ReadTimeout:  time.Second * 15,
+		WriteTimeout: time.Second * 15,
+		Addr:         ":8087",
+		Handler:      handles.SetupRoutes(*srcSecrt, *securty, *manager),
+	}
+
+	err := srvr.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
